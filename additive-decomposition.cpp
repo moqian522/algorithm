c1d3e884bbb736d1df1caaa131b56ece6f4bfc43
Heather <qian.mo@qq.com>
2021-09-15 18:41:37 +0800

    Fix bug in additive composition

--- additive-decomposition.cpp
+++ additive-decomposition.cpp
@@ -14,17 +14,35 @@ void printOutput(int number, int* decompositions,int decompositionCount) {
     cout << decompositions[i] << endl;
 }
 
-void decompseNumberDuplicated(int number,  int curDecomposeNumber, 
+void decompseNumberRepeated(int number,  int curDecomposeNumber, 
                               int* decompositions,int decompositionCount) {
     if (curDecomposeNumber == 0) {
         // Get one decompostion
         printOutput(number, decompositions, decompositionCount);
     }
     else {
-        for (int i = 1; i < curDecomposeNumber; i++)
+        for (int i = 1; i <= curDecomposeNumber; i++)
         {
             decompositions[decompositionCount] = i;
-            decompseNumberDuplicated(number, curDecomposeNumber - i, 
+            decompseNumberRepeated(number, curDecomposeNumber - i, 
+                                     decompositions, decompositionCount + 1);
+        }
+        
+    }           
+}
+
+void decompseNumberNotRepeated(int number,  int curDecomposeNumber, 
+                              int* decompositions,int decompositionCount) {
+    if (curDecomposeNumber == 0) {
+        // Get one decompostion
+        printOutput(number, decompositions, decompositionCount);
+    }
+    else {
+        int startIndex = decompositionCount > 0 ? decompositions[decompositionCount - 1] : 1;
+        for (int i = startIndex; i <= curDecomposeNumber; i++)
+        {
+            decompositions[decompositionCount] = i;
+            decompseNumberNotRepeated(number, curDecomposeNumber - i, 
                                      decompositions, decompositionCount + 1);
         }
         
@@ -33,5 +51,15 @@ void decompseNumberDuplicated(int number,  int curDecomposeNumber,
 
 int main()
 {
-    cout << "Hello World" << endl;
+    int n,m;
+	cin>>n>>m;
+	int number = n;
+	int* decompostions = new int[n];
+	if(m==1)
+		decompseNumberRepeated(number, n, decompostions, 0);
+	else if(m==2)
+		decompseNumberNotRepeated(number, n, decompostions, 0);
+    
+    delete decompostions;
+	return 0;
 }
